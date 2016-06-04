c24acdc9be9ca6da83ed29e9323c3ce5b65ce885
Bill Burke <billstron@gmail.com>
2016-06-04 14:01:55 -0400

    Added initil facebook api

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/webhook', require("./webhook").get)
+router.post('/webhook', require("./webhook").post)
+
 module.exports = router;
---
+++ routes/webhook.js
@@ -0,0 +1,27 @@
+
+var route = {};
+
+route.get = function (req, res, next) {
+  if (req.query['hub.verify_token'] === process.env.FB_VERIFYTOKEN) {
+    res.send(req.query['hub.challenge']);
+  }else{
+    res.send('Error, wrong validation token');
+  }
+};
+
+
+route.post = function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+    }
+  }
+  res.sendStatus(200);
+};
+
+
+module.exports = route;
